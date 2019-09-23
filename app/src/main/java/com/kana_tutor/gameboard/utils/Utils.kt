d37ddd7cd7420dda87ae6b69e7c0b4f5e607e4ac
sjs <sjs@sonic.net>
2019-09-23 11:19:54 -0700

    Clean-up.  Added some const to make code clearer and easier to mod if necessary.

--- app/src/main/java/com/kana_tutor/gameboard/utils/Utils.kt
+++ app/src/main/java/com/kana_tutor/gameboard/utils/Utils.kt
@@ -4,29 +4,29 @@ import android.content.res.Resources
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import com.kana_tutor.gameboard.MainActivity
-import com.kana_tutor.gameboard.Swipe
-import com.kana_tutor.gameboard.toDP
 import kotlin.math.abs
 
 val displayDensity = Resources.getSystem().displayMetrics.density
-fun Int.toDP() : Int {return (this.toFloat() / displayDensity).toInt()}
+fun Int.toDP() : Int = (toFloat() / displayDensity).toInt()
 
 enum class Swipe {
     UP, DOWN, LEFT, RIGHT, CLICK, NONE
 }
 
-class TouchEvent (xPix : Int = 0, yPix : Int = 0
+const val ACTION_NO_ACTION  = -1
+// Anything of longer duration is not a swipe or click.
+const val MAX_TOUCH_DURATION = 500 // milliseconds
+// minimum distance to define a swipe.  Anything shorter is a click.
+const val MIN_SWIPE_DISTANCE = 50 // dp
+class TouchEvent (val x : Int = 0, val y : Int = 0
                   , val time : Long = System.currentTimeMillis()) {
-    val x = xPix.toDP()
-    val y = yPix.toDP()
     override fun toString(): String {
         return String.format("x:%4d y:%4d time:%d", x, y, time)
     }
 }
 class TouchState {
-    private var start = TouchEvent(MainActivity.ACTION_NO_ACTION)
-    private var end = TouchEvent(MainActivity.ACTION_NO_ACTION)
+    private var start = TouchEvent(ACTION_NO_ACTION)
+    private var end = TouchEvent(ACTION_NO_ACTION)
     fun startEvent(x : Int, y : Int) {
         start = TouchEvent(x, y)
         end = TouchEvent()
@@ -34,28 +34,28 @@ class TouchState {
     fun endEvent(x: Int, y:Int) {
         end = TouchEvent(x, y)
     }
-    fun cancleEvent() {
+    fun cancelEvent() {
         start = TouchEvent()
         end = TouchEvent()
     }
-    fun deltaX()  = abs(start.x - end.x)
-    fun deltaY() = abs (start.y - end.y)
+    fun deltaX()  = abs(start.x - end.x).toDP()
+    fun deltaY() = abs (start.y - end.y).toDP()
     fun deltaT() = end.time - start.time
     fun swipeDirection() : Swipe {
-        var rv = Swipe.NONE
+        val rv: Swipe
         // event < 500 ms is no event
-        if (deltaT() < 500) {
+        if (deltaT() < MAX_TOUCH_DURATION) {
             // event distance < 50 dp is CLICK.
-            if (deltaX() > 50 || deltaY() > 50) {
+            rv = if (deltaX() > MIN_SWIPE_DISTANCE || deltaY() > MIN_SWIPE_DISTANCE) {
                 if (deltaX() > deltaY()) {
-                    rv = if (start.x > end.x) Swipe.LEFT else Swipe.RIGHT
+                    if (start.x > end.x) Swipe.LEFT else Swipe.RIGHT
                 }
                 else {
-                    rv = if (start.y > end.y) Swipe.UP else Swipe.DOWN
+                    if (start.y > end.y) Swipe.UP else Swipe.DOWN
                 }
             }
             else {
-                rv = Swipe.CLICK
+                Swipe.CLICK
             }
         }
         else {
@@ -73,19 +73,22 @@ fun swipeDetect (view: View, event : MotionEvent, state : TouchState) : Boolean 
     val action = event.actionMasked
     val x = event.rawX.toInt()
     val y = event.rawY.toInt()
-    if (action == MotionEvent.ACTION_DOWN) {
-        state.startEvent(x, y)
-    }
-    else if (action == MotionEvent.ACTION_UP) {
-        state.endEvent(x, y)
-        Log.d("TouchEvent",  "x:${state.deltaX()} "
-                + "y:${state.deltaY()} "
-                + "t:${state.deltaT()} "
-                + "swipe: ${state.swipeDirection()}")
-    }
-    else if (action != MotionEvent.ACTION_MOVE) {
-        state.cancleEvent()
-        rv = false
+    when (action) {
+        MotionEvent.ACTION_DOWN -> state.startEvent(x, y)
+        MotionEvent.ACTION_UP -> {
+            state.endEvent(x, y)
+            Log.d(
+                "TouchEvent", "x:${state.deltaX()} "
+                        + "y:${state.deltaY()} "
+                        + "t:${state.deltaT()} "
+                        + "swipe: ${state.swipeDirection()}"
+            )
+        }
+        MotionEvent.ACTION_MOVE -> {}
+        else -> { 
+            state.cancelEvent()
+            rv = false
+        }
     }
     return rv
 }
