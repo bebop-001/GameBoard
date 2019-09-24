1e824815696a0939afb64733790d283ef6a7af13
sjs <sjs@sonic.net>
2019-09-24 15:25:13 -0700

    game select is saved and restored.  It does not do anything yet though.

--- app/src/main/java/com/kana_tutor/gameboard/MainActivity.kt
+++ app/src/main/java/com/kana_tutor/gameboard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kana_tutor.gameboard
 
+import android.content.Context
 import android.graphics.Typeface
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,11 @@ import java.io.File
 import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PLAY_2048     = 0
+        const val PLAY_15   = PLAY_2048 + 1
+        var selectedGame = 0
+    }
     // Return a spanned html string using the appropriate call for
     // the user's device.
     private fun htmlString(htmlString:String) : Spanned {
@@ -62,11 +68,21 @@ class MainActivity : AppCompatActivity() {
             .show()
         return true
     }
+    fun newGameSelected(gameId : Int) {
+        selectedGame = gameId
+        getSharedPreferences("user_prefs.txt", Context.MODE_PRIVATE)
+            .edit()
+            .putInt("selectedGame", selectedGame)
+            .apply()
+    }
     // Menu item selected listener.
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         var rv = true
+
         when (item.itemId) {
             R.id.build_info_item -> displayAboutInfo()
+            R.id.game_15_select -> newGameSelected(PLAY_15)
+            R.id.game_2048_select -> newGameSelected(PLAY_2048)
             // If item isn't for this menu, you must call the super or
             // other things that must happen (eg: up-button in onSupportNavigateUp)
             // won't happen.
@@ -74,8 +90,20 @@ class MainActivity : AppCompatActivity() {
         return rv
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if (selectedGame == PLAY_2048)
+            menu!!.findItem(R.id.game_2048_select).setChecked(true)
+        else
+            menu!!.findItem(R.id.game_15_select).setChecked(true)
+
+        return super.onPrepareOptionsMenu(menu)
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
+        if (selectedGame == PLAY_2048)
+            menu!!.findItem(R.id.game_2048_select).setChecked(true)
+        else
+            menu!!.findItem(R.id.game_15_select).setChecked(true)
         return true
     }
 
@@ -83,7 +111,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.frag_container)
 
-
+        val prefs
+                = getSharedPreferences("user_prefs.txt", Context.MODE_PRIVATE)
+        selectedGame = prefs.getInt("selectedGame", PLAY_2048)
 
         supportFragmentManager
             .beginTransaction()
