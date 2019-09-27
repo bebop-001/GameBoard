b559c386e6f819c4fac87d1d696a5b79f06d06eb
sjs <sjs@sonic.net>
2019-09-27 11:02:30 -0700

    checkpoint -- live data is starting to work and game is starting to be playable.

--- app/src/main/java/com/kana_tutor/gameboard/game2048/Game2048Fragment.kt
+++ app/src/main/java/com/kana_tutor/gameboard/game2048/Game2048Fragment.kt
@@ -1,26 +1,25 @@
 package com.kana_tutor.gameboard.game2048
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.GridLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 
 import com.kana_tutor.gameboard.databinding.GameFragmentBinding
 import com.kana_tutor.gameboard.R
-import com.kana_tutor.gameboard.utils.Swipe
 import com.kana_tutor.gameboard.utils.SwipeDetector
 import com.kana_tutor.gameboard.utils.setGridAndButtonsSize
+import java.lang.RuntimeException
 
 
 class Game2048Fragment : Fragment() {
 
-    private var _swipe = MutableLiveData<Swipe>(Swipe.NONE)
-    val swipe : LiveData<Swipe>
-        get () = _swipe
     companion object {
         fun newInstance() = Game2048Fragment()
     }
@@ -30,7 +29,7 @@ class Game2048Fragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         var rv = true
         when (item.itemId) {
-            R.id.new_game_item -> {println("New Game")}
+            R.id.new_game_item -> {viewModel.newGame()}
             else -> rv = super.onOptionsItemSelected(item)
         }
         return rv
@@ -38,6 +37,40 @@ class Game2048Fragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.fragment_menu, menu)
+    }
+
+    fun updateView(positions:String, view : GridLayout) {
+        val cellValues = """(\d+|-)""".toRegex().findAll(positions).map{it.value}.toList()
+        if (cellValues.size != 16)
+            throw RuntimeException("updateGrid: Bad positions string: $positions\n"
+                + "Does not contain 16 values")
+        if (view.childCount != 16)
+            throw RuntimeException("updateGrid: Expected 16 views. found ${view.childCount}")
+        for (i in 0 .. (view.childCount - 1)) {
+            val tv = view.getChildAt(i) as TextView
+            tv.text = if (cellValues[i] == "-") "" else cellValues[i]
+            Log.d("tv update:", "$i: \"${tv.text}\"")
+        }
+        view.invalidate()
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(Game2048ViewModel::class.java)
+        viewModel.boardPositions.observe(
+            this
+            , Observer { updateView(it, view as GridLayout) }
+        )
+        viewModel.gameOver.observe(
+            this,
+            Observer {
+                if (it)
+                    Toast.makeText(this.context,"Game Over", Toast.LENGTH_LONG).show()
+            }
+        )
+
+
     }
 
     override fun onCreateView(
@@ -48,7 +81,6 @@ class Game2048Fragment : Fragment() {
             inflater, R.layout.game_fragment, container, false)
         binding.setLifecycleOwner(this)
 
-        viewModel = ViewModelProviders.of(this).get(Game2048ViewModel::class.java)
 
         val sd = SwipeDetector {viewModel.onSwipe(it)}
         container!!.setOnTouchListener{view, event -> sd.detect(view, event)}
--- app/src/main/java/com/kana_tutor/gameboard/game2048/Game2048ViewModel.kt
+++ app/src/main/java/com/kana_tutor/gameboard/game2048/Game2048ViewModel.kt
@@ -1,13 +1,50 @@
 package com.kana_tutor.gameboard.game2048
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import board.Direction
 import com.kana_tutor.gameboard.utils.Swipe
+import games.game2048.Game2048
+import games.game2048.newGame2048
 
 class Game2048ViewModel : ViewModel() {
+    companion object {
+        var game : Game2048 = newGame2048().initialize()
+    }
+    private var _boardPositions = MutableLiveData<String>(game.toString())
+    val boardPositions : LiveData<String>
+        get() = _boardPositions
+    private var _gameWon = MutableLiveData<Boolean>(false)
+    val gameWon : LiveData<Boolean>
+        get() = _gameWon
+    private var _gameOver = MutableLiveData<Boolean>(false)
+    val gameOver : LiveData<Boolean>
+        get() = _gameWon
 
-    var _swipeDetected = Swipe.NONE
+    fun newGame() : Game2048 {
+        _gameOver.value = false
+        _gameWon.value = false
+        game = newGame2048()
+        game.initialize()
+        _boardPositions.value = game.toString()
+        return game
+    }
     fun onSwipe(swipe : Swipe) {
-        println("Swipe:$swipe")
+        if (_gameOver.value == false) {
+            when(swipe) {
+                Swipe.LEFT -> game.processMove(Direction.LEFT)
+                Swipe.RIGHT -> game.processMove(Direction.RIGHT)
+                Swipe.UP -> game.processMove(Direction.UP)
+                Swipe.DOWN -> game.processMove(Direction.DOWN)
+                else -> {/* for now, ignore... */ }
+            }
+            _boardPositions.value = "$swipe:$game"
+            if (! game.canMove()) _gameOver.value = true
+            if (game.hasWon()) _gameWon.value = true
+        }
+        // Log.d("Swipe:", "$swipe -> ${_boardPositions.value}")
     }
 
 }
